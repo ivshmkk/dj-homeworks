0951edc81bc2ad8d1a0c447b8283f0bbeb4f9896
Ivan Shmikk <shmikk.v@yandex.ru>
2025-01-09 19:39:02 +0700

    initial commit

--- 2.1-databases/work_with_database/phones/management/commands/import_phones.py
+++ 2.1-databases/work_with_database/phones/management/commands/import_phones.py
@@ -1,7 +1,8 @@
 import csv
 
 from django.core.management.base import BaseCommand
-from phones.models import Phone
+from work_with_database.phones.models import Phone
+from django.utils.text import slugify
 
 
 class Command(BaseCommand):
@@ -13,5 +14,12 @@ class Command(BaseCommand):
             phones = list(csv.DictReader(file, delimiter=';'))
 
         for phone in phones:
-            # TODO: Добавьте сохранение модели
-            pass
+            Phone.objects.create(
+                id=phone['id'],
+                name=phone['name'],
+                image=phone['image'],
+                price=int(phone['price']),
+                release_date=phone['release_date'],
+                lte_exists=phone['lte_exists'],
+                slug=slugify(phone['name'])
+            )
--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -2,5 +2,10 @@ from django.db import models
 
 
 class Phone(models.Model):
-    # TODO: Добавьте требуемые поля
-    pass
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=50)
+    price = models.IntegerField(max_length=50)
+    image = models.URLField()
+    release_date = models.DateField()
+    lte_exists = models.BooleanField()
+    slug = models.SlugField(max_length=50)
